3425942ef50f37807c891548105f445f20f00bb1
Aaron Caswell <aaron@captureplay.com>
2021-05-26 10:13:00 -0700

    fix

--- src/BandwidthPayer.ts
+++ src/BandwidthPayer.ts
@@ -1,12 +1,11 @@
 export class BandwidthPayer {
   public readonly authorization: { actor: string; permission: string };
   constructor(
-    permission: string = "paybw",
     actor: string = "boost.wax",
+    permission: string = "paybw",
     public readonly account: string = "boost.wax",
     public readonly name: string = "noop"
   ) {
-    this.authorization.actor = actor;
-    this.authorization.permission = permission;
+    this.authorization = { actor, permission };
   }
 }
